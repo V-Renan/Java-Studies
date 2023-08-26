9e5a2d728f89f9f0bed7ab324acd26940b1a6e24
V-Renan <victorrenan2018@gmail.com>
2023-08-26 09:53:37 -0300

    collections no javinha

---
+++ src/br/com/victor/Modulo11/ExemploBubbleSortDinamico.java
@@ -0,0 +1,48 @@
+package br.com.victor.Modulo11;
+
+import java.util.Scanner;
+
+/**
+ * @author Victor$
+ * @date 26/08/2023$
+ * Description:
+ */
+public class ExemploBubbleSortDinamico {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite os números separados por [,]. ");
+
+        String resposta = scan.next();
+        String[] verSrt = resposta.split(",");
+        int[] vet = convert(verSrt);
+        int aux = 0;
+        int i = 0;
+
+        System.out.println("Vetor desordenado: ");
+        for (i=0; i<vet.length; i++) {
+            System.out.println(" " + vet[i]);
+        }
+        System.out.println(" ");
+
+        for (i=0; i< vet.length; i++) {
+            for (int j=0; j<vet.length-1; j++) {
+                if (vet[j] > vet[j + 1]) {
+                    aux = vet[j];
+                    vet[j] = vet[j+1];
+                    vet[j+1] = aux;
+                }
+            }
+        }
+
+    }
+
+    private static int[] convert(String[] vetStr) {
+        int[] numbers = new int[vetStr.length];
+        for (int i=0; i< vetStr.length; i++) {
+            numbers[i] = Integer.parseInt(vetStr[i]);
+        }
+        return numbers;
+    }
+}
