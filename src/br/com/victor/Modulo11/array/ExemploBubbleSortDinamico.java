fc0766b2903c13a0497c572a0e7d4ceddc29b522
V-Renan <victorrenan2018@gmail.com>
2023-08-28 16:40:19 -0300

    Corrigindo Array

--- src/br/com/victor/Modulo11/array/ExemploBubbleSortDinamico.java
+++ src/br/com/victor/Modulo11/array/ExemploBubbleSortDinamico.java
@@ -10,39 +10,44 @@ import java.util.Scanner;
 public class ExemploBubbleSortDinamico {
 
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Digite os números separados por ,  ");
+        Scanner scan = new Scanner(System.in);
 
-        String resposta = s.next();
+        System.out.print("Digite os números separados por [,]: ");
+        String resposta = scan.next();
+
         String[] vetStr = resposta.split(",");
         int[] vet = convert(vetStr);
         int aux = 0;
         int i = 0;
 
         System.out.println("Vetor desordenado: ");
-        for(i = 0; i<vet.length; i++){
-            System.out.println(" "+vet[i]);
+        for (i=0; i<vet.length; i++) {
+            System.out.println(" " + vet[i]);
         }
+
         System.out.println(" ");
 
-        for(i = 0; i<vet.length; i++){
-            for(int j = 0; j<vet.length-1; j++){
-                if(vet[j] > vet[j + 1]){
+        for (i=0; i<vet.length; i++) {
+            for (int j=0; j<vet.length-1; j++) {
+                if (vet[j] > vet[j + 1]) {
                     aux = vet[j];
-                    vet[j] = vet[j+1];
-                    vet[j+1] = aux;
+                    vet[j] = vet[j + 1];
+                    vet[j + 1] = aux;
                 }
             }
         }
+
         System.out.println("Vetor organizado:");
-        for(i = 0; i<vet.length; i++){
-            System.out.println(" "+vet[i]);
+        for (i=0; i<vet.length; i++) {
+            System.out.println(" " + vet[i]);
         }
+
+
     }
 
     private static int[] convert(String[] vetStr) {
         int[] numbers = new int[vetStr.length];
-        for(int i = 0;i < vetStr.length;i++) {
+        for (int i=0; i< vetStr.length; i++) {
             numbers[i] = Integer.parseInt(vetStr[i]);
         }
         return numbers;
