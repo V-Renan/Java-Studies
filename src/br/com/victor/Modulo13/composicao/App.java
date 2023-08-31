978922f8c12a22eec759651eb6c5941412f364c2
V-Renan <victorrenan2018@gmail.com>
2023-08-31 19:30:00 -0300

    composisao

--- src/br/com/victor/Modulo13/composicao/App.java
+++ src/br/com/victor/Modulo13/composicao/App.java
@@ -21,5 +21,12 @@ public class App {
         ContaPoupanca cp = new ContaPoupanca();
         cp.setBanco(banco);
         cp.setSaldo(10d);
+
+        banco.add(cc);
+        banco.add(cp);
+
+        Banco banco2 = new Banco();
+        banco.setCodigo(10L);
+        banco.setNome("Banco Victor");
     }
 }
