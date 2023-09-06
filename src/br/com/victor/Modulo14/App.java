bd77b16773eccf51b778b040b2e209cd0166b005
V-Renan <victorrenan2018@gmail.com>
2023-09-06 11:11:39 -0300

    Correção app

--- src/br/com/victor/Modulo14/App.java
+++ src/br/com/victor/Modulo14/App.java
@@ -1,6 +1,6 @@
 package br.com.victor.Modulo14;
 
-import br.com.victor.Modulo14.dao.ClienteMapDAO;
+import br.com.victor.Modulo14.dao.ClienteSetDAO;
 import br.com.victor.Modulo14.dao.IClienteDAO;
 import br.com.victor.Modulo14.domain.Cliente;
 
@@ -8,102 +8,109 @@ import javax.swing.*;
 
 /**
  * @author Victor$
- * @date 9/5/2023$
+ * @date 9/6/2023$
  * Description:
  */
 public class App {
 
     private static IClienteDAO iClienteDAO;
 
-    public static void main(String[] args) {
-        iClienteDAO = new ClienteMapDAO();
+    public static void main(String args[]) {
+        iClienteDAO = new ClienteSetDAO();
 
-        String opcao = JOptionPane.showInputDialog(null, "Digite 1 para cadastro, 2 para consultar, 3 para exclusão, 4 para alteração ou 5 para sair", "Cadastro", JOptionPane.INFORMATION_MESSAGE);
+        String opcao = JOptionPane.showInputDialog(null,
+                "Digite 1 para cadastro, 2 para consultar, 3 para exclusão, 4 para alteração ou 5 para sair",
+                "Green dinner", JOptionPane.INFORMATION_MESSAGE);
 
         while (!isOpcaoValida(opcao)) {
             if ("".equals(opcao)) {
                 sair();
             }
-            opcao = JOptionPane.showInputDialog(null, "Opção inválida digite 1 para cadastro, 2 para consulta, 3 para exclusão, 4 para alteração ou 5 para sair", "Green dinner", JOptionPane.INFORMATION_MESSAGE);
+            opcao = JOptionPane.showInputDialog(null,
+                    "Opção inválida digite 1 para cadastro, 2 para consulta, 3 para cadastro, 4 para alteração ou 5 para sair",
+                    "Green dinner", JOptionPane.INFORMATION_MESSAGE);
         }
 
         while (isOpcaoValida(opcao)) {
+
             if (isOpcaoSair(opcao)) {
                 sair();
-            } else if (isCadastro(opcao)) {
-                String dados = JOptionPane.showInputDialog(null, "Digite os dados do cliente separados por vírgula conforme exemplo: Nome, CPF, Telefone, Endereço, número, Cidade, Estado", "Cadastro", JOptionPane.INFORMATION_MESSAGE);
+            }else if (isCadastro(opcao)) {
+                String dados = JOptionPane.showInputDialog(null,
+                        "Digite os dados do cliente separados por vígula, conforme exemplo: Nome, CPF, Telefone, Endereço, Número, Cidade e Estado",
+                        "Cadastro", JOptionPane.INFORMATION_MESSAGE);
                 cadastrar(dados);
-            } else if (isConsultar(opcao)) {
-                String dados = JOptionPane.showInputDialog(null, "Digite o cpf", "Consultar", JOptionPane.INFORMATION_MESSAGE);
+            } else if (isConsulta(opcao)) {
+                String dados = JOptionPane.showInputDialog(null,
+                        "Digite o CPF do cliente",
+                        "Consulta cliente", JOptionPane.INFORMATION_MESSAGE);
                 consultar(dados);
-            } else if (isOpcaoExluir(opcao)) {
-                String dados = JOptionPane.showInputDialog(null, "Digite o cpf", "Excluir", JOptionPane.INFORMATION_MESSAGE);
+            } else if (isExclusao(opcao)) {
+                String dados = JOptionPane.showInputDialog(null,
+                        "Digite o CPF do cliente",
+                        "Consulta cliente", JOptionPane.INFORMATION_MESSAGE);
                 excluir(dados);
-            } else if (isAtualizar(opcao)) {
-                String dados = JOptionPane.showInputDialog(null, "Digite os dados do cliente separados por vírgula, conforme exmeplo: Nome, CPF, Telefone, Enderenço, Número, Cidade, Estado", "Atualização", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                String dados = JOptionPane.showInputDialog(null,
+                        "Digite os dados do cliente separados por vígula, conforme exemplo: Nome, CPF, Telefone, Endereço, Número, Cidade e Estado",
+                        "Atualização", JOptionPane.INFORMATION_MESSAGE);
                 atualizar(dados);
             }
 
+            opcao = JOptionPane.showInputDialog(null,
+                    "Digite 1 para cadastro, 2 para consulta, 3 para cadastro, 4 para alteração ou 5 para sair",
+                    "Green dinner", JOptionPane.INFORMATION_MESSAGE);
 
-            opcao = JOptionPane.showInputDialog(null, "Digite 1 para cadastro, 2 para consulta, 3 para exclusão, 4 para alteração ou 5 para sair", "Cadastro", JOptionPane.INFORMATION_MESSAGE);
         }
 
-        opcao = JOptionPane.showInputDialog(null, "Digite 1 para cadastro, 2 para consulta, 3 para exclusão, 4 para alteração ou 5 para sair", "Cadastro", JOptionPane.INFORMATION_MESSAGE);
     }
 
-    private static boolean isAtualizar(String opcao) {
-        if ("4".equals(opcao)) {
-            return true;
-        }
-        return false;
-    }
-
-    public static void atualizar(String dados) {
+    private static void atualizar(String dados) {
         String[] dadosSeparados = dados.split(",");
-        Cliente cliente = new Cliente(dadosSeparados[0], dadosSeparados[1], dadosSeparados[2], dadosSeparados[3], dadosSeparados[4], dadosSeparados[5], dadosSeparados[6]);
+        Cliente cliente = new Cliente(dadosSeparados[0],dadosSeparados[1],dadosSeparados[2],dadosSeparados[3],dadosSeparados[4],dadosSeparados[5],dadosSeparados[6]);
         iClienteDAO.alterar(cliente);
     }
 
-    private static boolean isOpcaoExluir(String opcao) {
+    private static void excluir(String dados) {
+        iClienteDAO.excluir(Long.parseLong(dados));
+        JOptionPane.showMessageDialog(null, "Cliente excluído com sucesso: ", "Sucesso",JOptionPane.INFORMATION_MESSAGE);
+    }
+
+
+    private static void consultar(String dados) {
+        Cliente cliente = iClienteDAO.consultar(Long.parseLong(dados));
+        if (cliente != null) {
+            JOptionPane.showMessageDialog(null, "Cliente encontrado com sucesso: " + cliente.toString(), "Sucesso",JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(null, "Cliente não encontrado", "ERRO",JOptionPane.INFORMATION_MESSAGE);
+        }
+
+    }
+
+    private static void cadastrar(String dados) {
+        String[] dadosSeparados = dados.split(",");
+        Cliente cliente = new Cliente(dadosSeparados[0],dadosSeparados[1],dadosSeparados[2],dadosSeparados[3],dadosSeparados[4],dadosSeparados[5],dadosSeparados[6]);
+        Boolean isCadastrado = iClienteDAO.cadastrar(cliente);
+        if (isCadastrado) {
+            JOptionPane.showMessageDialog(null, "Cliente cadastrado com sucesso ", "Sucesso",JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(null, "Cliente já se encontra cadastrado", "Erro",JOptionPane.INFORMATION_MESSAGE);
+        }
+
+    }
+
+    private static boolean isExclusao(String opcao) {
         if ("3".equals(opcao)) {
             return true;
         }
         return false;
     }
 
-    public static void excluir(String dados) {
-        iClienteDAO.excluir(Long.parseLong(dados));
-        JOptionPane.showMessageDialog(null,"Cliente excluído com sucesso: ", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
-    }
-
-    public static void consultar(String dados) {
-        Cliente cliente = iClienteDAO.consultar(Long.parseLong(dados));
-        if (cliente != null) {
-            JOptionPane.showMessageDialog(null, "Cliente encontrado: " + cliente.toString(), "Sucesso", JOptionPane.INFORMATION_MESSAGE);
-        } else {
-            JOptionPane.showMessageDialog(null, "Cliente não encontrado: ", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
-        }
-    }
-
-    public static boolean isConsultar(String opcao) {
+    private static boolean isConsulta(String opcao) {
         if ("2".equals(opcao)) {
             return true;
         }
         return false;
-    }
-
-    private static void cadastrar(String dados) {
-        String[] dadosSeparados = dados.split(",");
-        //Tentar validar se todos os campos estão preenchidos.
-        //Se não tiver, passar null no construtor onde o valor é nulo
-        //Cliente cliente = new Cliente(dadosSeparados[0],dadosSeparados[1],null,dadosSeparados[3],dadosSeparados[4],dadosSeparados[5],dadosSeparados[6])
-        Cliente cliente = new Cliente(dadosSeparados[0], dadosSeparados[1], dadosSeparados[2], dadosSeparados[3], dadosSeparados[4], dadosSeparados[5], dadosSeparados[6]);
-        Boolean isCadastrado = iClienteDAO.cadastrar(cliente);
-        if (isCadastrado) {
-            JOptionPane.showMessageDialog(null, "Cliente cadastrado com sucesso", "sucesso", JOptionPane.INFORMATION_MESSAGE);
-        } else {
-            JOptionPane.showMessageDialog(null, "Cliente já se encontra cadastrado", "ERRO", JOptionPane.INFORMATION_MESSAGE);
-        }
     }
 
     private static boolean isCadastro(String opcao) {
@@ -113,6 +120,16 @@ public class App {
         return false;
     }
 
+    private static void sair() {
+        String clientesCadastrados = "";
+        for (Cliente cliente : iClienteDAO.buscarTodos()) {
+            clientesCadastrados += cliente.toString() + "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, "Clientes cadastrados: " + clientesCadastrados, "Até logo",JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
+
     private static boolean isOpcaoSair(String opcao) {
         if ("5".equals(opcao)) {
             return true;
@@ -120,24 +137,11 @@ public class App {
         return false;
     }
 
-    private static void sair() {
-        JOptionPane.showMessageDialog(null, "Até logo: ", "Sair", JOptionPane.INFORMATION_MESSAGE);
-        System.exit(0);
-    }
-
     private static boolean isOpcaoValida(String opcao) {
-        if ("1".equals(opcao) || "2".equals(opcao) || "3".equals(opcao) || "4".equals(opcao) || "5".equals(opcao)) {
+        if ("1".equals(opcao) || "2".equals(opcao)
+                || "3".equals(opcao) || "4".equals(opcao) || "5".equals(opcao)) {
             return true;
         }
         return false;
     }
-
-    private static boolean isopcaoCadastro(String opcao) {
-        if ("1".equals(opcao)) {
-            return true;
-        }
-        return false;
-    }
-
-
 }
